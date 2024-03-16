b50a3693d4534a803876e8cab79a6d7392b26f8f
John Kay <jkernusz1346@gmail.com>
2024-03-15 21:02:49 -0600

    added pages to mainjsx file

--- celcurl/src/main.jsx
+++ celcurl/src/main.jsx
@@ -2,9 +2,57 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Info from './pages/Info.jsx';
+import Services from './pages/Services.jsx';
+import Products from './pages/Products.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1>404</h1>,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      },
+      {
+        path: '/info',
+        element: <Info />
+      },
+      {
+        path: '/services',
+        element: <Services />
+      },
+      {
+        path: '/products',
+        element: <Products />
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio />
+      },
+    ],
+
+  },
+]);
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router}/>
+);
